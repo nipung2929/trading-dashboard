589ad16f9a0f84703694af0ca7cdfd6bdbe7ab4e
Nipun Gupta <nipun@Nipuns-MacBook-Air.local>
2025-07-18 23:51:45 +0530

    Added orders display

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Orders = () => {
-  return (
-    <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+  const [allOrders, setAllOrders] = useState([]);
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
+  useEffect(() => {
+    axios.get("https://api.digitalwallethub.site/allOrders").then((res) => {
+      setAllOrders(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h3 className="title">Orders ({allOrders.length})</h3>
+      <div className="order-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Qty</th>
+              <th>Price</th>
+              <th>Mode</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allOrders.map((order, index) => (
+              <tr key={index}>
+                <td>{order.name}</td>
+                <td>{order.qty}</td>
+                <td>{order.price}</td>
+                <td>{order.mode}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {allOrders.length === 0 && (
+          <div className="no-orders">
+            <p>You haven't placed any orders today</p>
+            <Link to={"/"} className="btn">
+              Get started
+            </Link>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
